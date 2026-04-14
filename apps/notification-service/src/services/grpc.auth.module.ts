8b6dc79f401e5df64983d306958ec3537923af02
Mạch Ngọc Xuân <machngocxuan2004@gmail.com>
2026-04-15 01:29:05 +0700

    update docker env production

--- apps/notification-service/src/services/grpc.auth.module.ts
+++ apps/notification-service/src/services/grpc.auth.module.ts
@@ -2,7 +2,15 @@ import { Module } from '@nestjs/common';
 import { GrpcAuthService } from './grpc.auth.service';
 import { GrpcModule } from 'nestjs-grpc';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { existsSync } from 'fs';
 import { join } from 'path';
+
+const protoPath = (() => {
+  const distPath = join(process.cwd(), 'dist/src/protos/auth.proto');
+  const srcPath = join(process.cwd(), 'src/protos/auth.proto');
+
+  return existsSync(distPath) ? distPath : srcPath;
+})();
 
 @Module({
   imports: [
@@ -11,8 +19,7 @@ import { join } from 'path';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         package: configService.get<string>('grpc.authPackage', 'auth'),
-        // protoPath: join(__dirname, '../../protos/auth.proto'), // => pro
-        protoPath: join(process.cwd(), 'src/protos/auth.proto'), // => dev
+        protoPath,
         url: configService.get<string>('grpc.authUrl', '0.0.0.0:50051'),
         serviceName: 'AuthService',
       }),
