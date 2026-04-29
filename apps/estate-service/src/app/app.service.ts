83c2a5b98bc3fe096d221744db223e5ade9d2916
Mạch Ngọc Xuân <machngocxuan2004@gmail.com>
2026-04-29 23:27:16 +0700

    Test deploy estate with cicd

--- apps/estate-service/src/app/app.service.ts
+++ apps/estate-service/src/app/app.service.ts
@@ -4,6 +4,6 @@ import { MessageKey } from 'src/common/decorators/message.decorator';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Mach Ngoc Xuan test';
+    return 'Mach Ngoc Xuan test ci/cd';
   }
 }
